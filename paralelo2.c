9bb58be605a30d6c1e29b9863af1f524aad1984a
Gustavo Mendonça Dias <gmdiias@gmail.com>
2018-11-17 10:53:25 -0200

    testes de arquivo threads paralelos

--- paralelo2.c
+++ paralelo2.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <pthread.h>
+#include <stdlib.h>
 
-/*
-*	Trabalho de Compiladores Floyd Warshall
-*	Alunos:
-*		Frank Tamborelli		RA:
-*		Gustavo Mendonca Dias	RA: 88410
-*		Matheus Colares 		RA: 
-*		Tatiane Paz				RA: 
-*/
-
+#define NTH 4
 #define MIN(x, y) (((x) < (y)) ? (x) : (y)) // Calcula minimo entre dos valores
 #define CAMINHOARQUIVO "Teste/grafo_500.g" // Define caminho do arquivo
-
+int arrive[NTH];
+int cont[NTH];
+int tamMatriz = 0;
 float **mat;
 float **matB;
-int tamMatriz;
-int i, j, k;
+int linhasThreads = 0;
 
 // Funcao para alocar matriz na memoria
 float **alocmat(int lin, int col){
+	int i;
 	float **m;
 	m=(float**)malloc(sizeof(float*)*lin);
 	for(i=0;i<lin;i++){
@@ -31,6 +24,7 @@ float **alocmat(int lin, int col){
 }
 
 void inicializaMatriz(int tamMatriz, float **mat){
+	int i, j;
 	for(i=1;i<tamMatriz;i++){
 		for(j=1;j<tamMatriz;j++){
 			if(i == j) {
@@ -43,7 +37,8 @@ void inicializaMatriz(int tamMatriz, float **mat){
 	}
 }
 
-void imprimeMatriz(){
+void imprimeMatriz(int tamMatriz, float **mat){
+	int i, j;
 	for(i=1;i<tamMatriz;i++){
 		for(j=1;j<tamMatriz;j++){
 			printf("%f \t", mat[i][j]);
@@ -52,29 +47,22 @@ void imprimeMatriz(){
 	}
 }
 
-void *thread_hello(void *arg){
-	int coluna;
-	int linha = (int)arg;
-	// printf("executando thread k = %i, linha = %i, coluna = %i\n", k, linha, coluna);
-	for(coluna=1; coluna<tamMatriz; coluna++){
-		mat[linha][coluna] = MIN(mat[linha][coluna], matB[linha][k] + matB[k][coluna]);
-	}
-	pthread_exit(0);
-}
-
-// Calcula menor caminho entre os vertices da matriz
-void floydWarshall(){
-	pthread_t *t=(pthread_t*)malloc(tamMatriz*sizeof(pthread_t));
-	for(k=1; k<tamMatriz; k++){
-		matB = mat;
-		// printf("%i\n", k);
-		for(i=1; i<tamMatriz; i++){
-			//printf("criei a thread %i\n", i);
-			pthread_create(&t[i],NULL,thread_hello,(void*)i);
+void *do_job(void *arg){
+	int tid = (int)arg;
+	int i, j, k;
+	int maxLinha = ((int)arg+1) * linhasThreads;
+	int minLinha = maxLinha - linhasThreads;
+	//imprimeMatriz(tamMatriz, matB);
+	for(k=1;k<tamMatriz;k++){
+		//printf("sou uma thread cuido da linha %i ate a %i na altura %i tamanho %i \n", minLinha, maxLinha, k, tamMatriz);
+		for(i=minLinha; i<=maxLinha; i++){
+			for(j=1; j<tamMatriz; j++){
+				mat[i][j] = MIN(matB[i][j], matB[i][k] + matB[k][j]);
+			}
 		}
-		for(i=1; i<tamMatriz; i++){
-			pthread_join(t[i],NULL);
-		}
+		arrive[tid]=1;
+		while(cont[tid]!=1);
+		cont[tid]=0;
 	}
 }
 
@@ -98,7 +86,6 @@ int main(){
 
 	tamMatriz = atoi(penultimaLinha)+1; // Transforma tamanho da matriz para int
 
-	// Aloca memoria para matrizes
 	mat=alocmat(tamMatriz,tamMatriz);
 	matB=alocmat(tamMatriz,tamMatriz);
 
@@ -112,16 +99,37 @@ int main(){
 		fscanf(arquivo, "%i", &posY);
 		fscanf(arquivo, "%f", &peso);
 		fgets(linha, 100, arquivo);
-		mat[posX][posY] = peso;
+		matB[posX][posY] = peso;
   	}
+    // imprimeMatriz(tamMatriz, matB);
+
+	linhasThreads = (tamMatriz-1) / NTH;
 	clock_t Ticks[2];
 	Ticks[0] = clock();
-	floydWarshall(tamMatriz, mat, matB);
+	pthread_t t[NTH];
+	int n, i;
+	for(i=0;i<NTH;i++){
+		arrive[i]=0;
+		cont[i]=0;
+		pthread_create(&t[i],NULL,do_job,(void*)i);
+	}
+	for(n=0;n<tamMatriz-1;n++){
+		for(i=0;i<NTH;i++){
+			while(arrive[i]!=1);
+			arrive[i]=0;
+		}
+		// printf("%i\n", n);
+		matB = mat;
+		for(i=0;i<NTH;i++){
+			cont[i]=1;
+		}
+	}		
 	Ticks[1] = clock();
 	double Tempo = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
 	printf("Tempo gasto: %g ms.", Tempo);
-	gets();
-
+	getchar();
 	imprimeMatriz(tamMatriz, mat);
+
+	pthread_exit(NULL);
 
 }
---
+++ sequencial.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/*
+*	Trabalho de Compiladores Floyd Warshall
+*	Alunos:
+*		Frank Tamborelli		RA:
+*		Gustavo Mendonca Dias	RA: 88410
+*		Matheus Colares 		RA: 
+*		Tatiane Paz				RA: 
+*/
+
+#define MIN(x, y) (((x) < (y)) ? (x) : (y)) // Calcula minimo entre dos valores
+#define CAMINHOARQUIVO "Teste/grafo_500.g" // Define caminho do arquivo
+
+// Funcao para alocar matriz na memoria
+float **alocmat(int lin, int col){
+	int i;
+	float **m;
+	m=(float**)malloc(sizeof(float*)*lin);
+	for(i=0;i<lin;i++){
+		m[i]=(float*)malloc(sizeof(float)*col);
+	}
+	return m;
+}
+
+void inicializaMatriz(int tamMatriz, float **mat){
+	int i, j;
+	for(i=1;i<tamMatriz;i++){
+		for(j=1;j<tamMatriz;j++){
+			if(i == j) {
+				mat[i][j] = 0;
+			}
+			else {
+				mat[i][j] = 99999;
+			}
+		}
+	}
+}
+
+void imprimeMatriz(int tamMatriz, float **mat){
+	int i, j;
+	for(i=1;i<tamMatriz;i++){
+		for(j=1;j<tamMatriz;j++){
+			printf("%f \t", mat[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+// Calcula menor caminho entre os vertices da matriz
+void floydWarshall(int tamMatriz, float **mat, float **matB){
+	int i, j, k;
+	for(k=1; k<tamMatriz; k++){
+		matB = mat;
+		// printf("%i\n", k);
+		for(i=1; i<tamMatriz; i++){
+			for(j=1; j<tamMatriz; j++){
+				mat[i][j] = MIN(matB[i][j], matB[i][k] + matB[k][j]);
+			}
+		}
+	}
+}
+
+int main(){
+	FILE *arquivo;
+	char linha[100], penultimaLinha[100];
+	int k, i, j, tamMatriz, posX, posY;
+  	float peso;
+
+	// Realiza a abertura do arquivo
+	if ((arquivo = fopen(CAMINHOARQUIVO, "r")) == NULL){
+        printf("Erro na abertura do arquivo \n");
+        exit(1);
+    }
+    
+	// Obtem tamanho da matriz
+	do {
+		strcpy(penultimaLinha, linha);
+		fgets(linha, 100, arquivo);
+	} while (strcmp(linha, "#arestas\n") != 0);
+
+	tamMatriz = atoi(penultimaLinha)+1; // Transforma tamanho da matriz para int
+
+	// Aloca memoria para matrizes
+	float **mat=alocmat(tamMatriz,tamMatriz);
+	float **matB=alocmat(tamMatriz,tamMatriz);
+
+	// Inicializa as matrizes
+	inicializaMatriz(tamMatriz, mat);
+	inicializaMatriz(tamMatriz, matB);
+
+	// Realiza a leitura dos pesos de cada vertice
+	while (!feof(arquivo)){
+    	fscanf(arquivo, "%i", &posX); 
+		fscanf(arquivo, "%i", &posY);
+		fscanf(arquivo, "%f", &peso);
+		fgets(linha, 100, arquivo);
+		mat[posX][posY] = peso;
+  	}
+	clock_t Ticks[2];
+	Ticks[0] = clock();
+	floydWarshall(tamMatriz, mat, matB);
+	Ticks[1] = clock();
+	double Tempo = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
+	printf("Tempo gasto: %g ms.", Tempo);
+	getchar();
+	imprimeMatriz(tamMatriz, mat);
+
+}
